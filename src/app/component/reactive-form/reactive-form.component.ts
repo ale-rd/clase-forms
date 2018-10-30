fbbcddd8ea3f14b26a350a115c0970e6befb66c7
Your Name <you@example.com>
2018-10-29 21:48:52 -0300

    fin clase de forms reactivos

--- src/app/component/reactive-form/reactive-form.component.ts
+++ src/app/component/reactive-form/reactive-form.component.ts
@@ -1,4 +1,4 @@
-import {Validators, FormGroup,  FormBuilder} from '@angular/forms';
+import {AbstractControl, Validators,  FormGroup,   FormBuilder} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,16 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class ReactiveFormComponent implements OnInit {
 
   myForm: FormGroup;
+  public apellido: AbstractControl;
+  public newsCheck: AbstractControl;
   
    constructor(private fb: FormBuilder) {
        this.myForm = fb.group({
          'nombre': ['', Validators.required ],  // valor por defecto
-         'apellido': [''],
-         'email':['']
+         'apellido': ['', Validators.required],
+        //  'email':['', [Validators.required, Validators.email]]
+        'email':[{value:'', disabled: true}, Validators.email],
+        'newsCheck':['']
        });
+       this.apellido = this.myForm.controls['apellido'];
+       this.newsCheck = this.myForm.controls['newsCheck'];
+
+       this.newsCheck.valueChanges.subscribe( (value: boolean) =>{
+        if(value){
+          this.email.enable();
+        }else{
+          this.email.disable();
+        }
+      })
      }
   
   ngOnInit() {
+  }
+
+  
+  // Otra forma, con getter
+  get nombre(){ 
+    return this.myForm.get('nombre');
+  }
+
+  get email(){
+    return this.myForm.get('email');
   }
 
 }
